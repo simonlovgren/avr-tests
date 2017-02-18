38e7cf8263a324d173a13e151a5661beedaa1b1a
Simon Lövgren <lovgren.simon@gmail.com>
2017-02-18 22:46:54 +0100

    Changed blink to 9.6MHz

--- attiny13a/blink/blink.cpp
+++ attiny13a/blink/blink.cpp
@@ -1,15 +1,20 @@
 #ifndef f_CPU
-#define F_CPU 1200000UL
+#define F_CPU 9600000UL
 #endif
 
 #include <avr/io.h>
 #include <util/delay.h>
 
 int main(void) {
+  // Configure PORTB-pins
   DDRB = 0x10;
+
+  // Loop forever
   while(1) {
+    // LED On
     PORTB = 0b00010000;
     _delay_ms(500);
+    // LED Off
     PORTB = 0b00000000;
     _delay_ms(500);
   }
