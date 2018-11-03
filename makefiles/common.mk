3a0515146358d1aedd9845faf4d78906b7fe2c04
Simon Lövgren <lovgren.simon@gmail.com>
2018-11-03 18:31:12 +0100

    Now allows overriding PORT and PROGRAMMER from command line.

--- makefiles/common.mk
+++ makefiles/common.mk
@@ -1,21 +1,21 @@
 # Globally common variables
-PROGRAMMER := buspirate
+PROGRAMMER ?= buspirate
 
 # Detect OS and set correct variables
 ifeq ($(OS),Windows_NT)
 	# Windows
-	PORT := COM3
+	PORT ?= COM3
 	RM   := del
 else
 	UNAME_S := $(shell uname -s)
 	ifeq ($(UNAME_S),Linux)
 		# Linux
-		PORT := /dev/ttyUSB0
+		PORT ?= /dev/ttyUSB0
 		RM   := rm -f
 	endif
 	ifeq ($(UNAME_S),Darwin)
 		# OSX
-		PORT := /dev/ttyUSB0
+		PORT ?= /dev/ttyUSB0
 		RM   := rm -f
 	endif
 endif
